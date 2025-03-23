c2f28d4a49a2966f296b465e008d4f20ba6050be
Ricky Stone <garyth3duck8@gmail.com>
2025-03-23 00:37:43 +0000

    Make SwiftRestRequest initializer and methods public

--- Sources/SwiftRest/SwiftRestRequest.swift
+++ Sources/SwiftRest/SwiftRestRequest.swift
@@ -13,22 +13,22 @@ public final class SwiftRestRequest {
     private(set) var parameters: [String: String]?
     private(set) var jsonBody: String?
     
-    init(path: String, method: HTTPMethod) {
+    public init(path: String, method: HTTPMethod) {
         self.path = path
         self.method = method
     }
     
-    func addHeader(_ key: String, _ value: String) {
+    public func addHeader(_ key: String, _ value: String) {
         if headers == nil { headers = [:] }
         headers?[key] = value
     }
     
-    func addParameter(_ key: String, _ value: String) {
+    public func addParameter(_ key: String, _ value: String) {
         if parameters == nil { parameters = [:] }
         parameters?[key] = value
     }
     
-    func addJsonBody<T: Encodable>(_ object: T) throws {
+    public func addJsonBody<T: Encodable>(_ object: T) throws {
         self.jsonBody = try Json.toString(object: object)
     }
 }
