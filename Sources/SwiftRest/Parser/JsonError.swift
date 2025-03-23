a1b6bac0c1a8e6af7e6a6cab3708fba6352954f8
Ricky Stone <garyth3duck8@gmail.com>
2025-03-23 00:10:44 +0000

    Refactor: Rename folder to "Parse" to resolve duplicate Json.swift conflict

---
+++ Sources/SwiftRest/Parser/JsonError.swift
@@ -0,0 +1,11 @@
+//
+//  Swift Rest
+//  Created by Ricky Stone on 22/03/2025.
+//
+
+public enum JsonError: Error {
+    case dataConversionFailed
+    case decodingFailed
+    case encodingFailed
+    case emptyResult
+}
